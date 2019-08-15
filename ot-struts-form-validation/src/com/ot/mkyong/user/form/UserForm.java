4e8e64a8a18c5351325012940c9d20a69c95d894
Marlon Monzon <martianworm17@gmail.com>
2019-08-15 10:28:58 +0800

    Development savepoint

---
+++ ot-struts-form-validation/src/com/ot/mkyong/user/form/UserForm.java
@@ -0,0 +1,41 @@
+package com.ot.mkyong.user.form;
+
+public class UserForm {
+	
+	private String username;
+	private String pwd;
+	private String pw2;
+	private String email;
+	
+	public String getUsername() {
+		return username;
+	}
+	
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	
+	public String getPwd() {
+		return pwd;
+	}
+	
+	public void setPwd(String pwd) {
+		this.pwd = pwd;
+	}
+	
+	public String getPw2() {
+		return pw2;
+	}
+	
+	public void setPw2(String pw2) {
+		this.pw2 = pw2;
+	}
+	
+	public String getEmail() {
+		return email;
+	}
+	
+	public void setEmail(String email) {
+		this.email = email;
+	}
+}
